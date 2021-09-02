52a3e5d3c9d33f53fe545178100276a0726a11a4
theresfuelinmycoffee <atigrouse.com>
2021-09-01 18:27:16 -0700

    added loading image to video

--- src/Components/HeroVideo/index.js
+++ src/Components/HeroVideo/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import bgVideo from '../../assets/bgVideo.mp4';
 import style from './styles.module.css';
+import posterImg from '../../assets/bat-ray.jpeg';
 
 function HeroVideo() {
   return (
     <div className={style.heroMain}>
-      <video className={style.heroPlayer} loop autoPlay muted>
+      <video className={style.heroPlayer} poster={posterImg} loop autoPlay muted>
         <source src={bgVideo} type="video/mp4" />
         <source src={bgVideo} type="video/oog" />
       </video>
