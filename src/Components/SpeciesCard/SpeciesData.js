7d940bf8704a0030fdf7fe4b5b5ad034414383c3
theresfuelinmycoffee <atigrouse.com>
2021-11-23 11:59:56 -0800

    added regulation info

--- src/Components/SpeciesCard/SpeciesData.js
+++ src/Components/SpeciesCard/SpeciesData.js
@@ -18,9 +18,10 @@ export const SpeciesData = [
     longDescription: 'still under research',
     profileImage: leopardShark,
     diet: 'still under research',
-    catchMethod: 'still under research',
-    regulation: 'still under research',
-    source: 'still under research',
+    catchMethod: 'High low Rig with sand crab, squid, or other cut bait is popular',
+    regulation:
+      'The recreational fishery for leopard shark (Triakis semifasciata) is open year-round to divers and shore-based anglers. This fishery inside San Diego Bay, Mission Bay, Newport Bay and Alamitos Bay is open year-round to boat-based anglers.Outside of the above-mentioned embayments, this fishery is open to boat-based anglers from March 1, 2021 through December 31, 2021. Take of leopard shark is prohibited seaward of the 100 fathom (600 feet) Rockfish Conservation Area boundary line, which is a series of connected waypoints defined in Federal regulations (50 CFR Part 660, Subpart C). Note that leopard shark may not be taken in the Cowcod Conservation Areas. The daily bag and possession limit is 3 fish with a minimum size limit of 36 inches total length. The leopard shark is part of a group of fish known as groundfish, which includes over 90 species that live on or near the bottom of the ocean (with a few exceptions). Summary Table of Ground Fish regulations: https://wildlife.ca.gov/Fishing/Ocean/Regulations/Groundfish-Summary#south , Additional ground fish info: https://wildlife.ca.gov/Fishing/Ocean/Regulations/Fishing-Map/Southern#groundfishinfo  ',
+    source: 'https://wildlife.ca.gov/Fishing/Ocean/Regulations/Fishing-Map/Southern#leopard',
   },
   {
     id: 'Barred Surfperch',
@@ -33,8 +34,10 @@ export const SpeciesData = [
 
     diet: 'still under research',
     catchMethod: 'still under research',
-    regulation: 'still under research',
-    source: 'still under research',
+    regulation:
+      'The recreational fishery for surfperch (family Embiotocidae) is open year-round. The daily bag and possession limit is 20 fish in combination of all species (except shiner perch), with not more than 10 fish of any one species. Shiner perch (Cymatogaster aggregata) have a separate bag and possession limit of 20 fish. Redtail surfperch (Amphistichus rhodoterus) have a minimum size limit of 10½ inches total length.',
+    source:
+      'https://wildlife.ca.gov/Fishing/Ocean/Regulations/Fishing-Map/Southern#surfperch , Indentification guide: https://nrm.dfg.ca.gov/FileHandler.ashx?DocumentID=36617&inline',
   },
 
   {
@@ -48,7 +51,7 @@ export const SpeciesData = [
       ' The Corbina is marine demersal fish in the croaker family. It can often be found along sandy beaches and in shallow bays. This species travels in small groups along the surf zone in a few inches of water to depths of 20 meters (66 ft). Other names include "California kingcroaker," "California whiting," and "kingfish".',
     profileImage: californiaCorbina,
     diet: 'crustaceans, small fish, bivalves, and other small invertebrates',
-    catchMethod: 'sight casting on light tackle is a popular method',
+    catchMethod: 'sight casting with light tackle is a popular method',
     regulation: 'still under research',
     source: 'still under research',
   },
@@ -62,9 +65,11 @@ export const SpeciesData = [
     longDescription: '',
     profileImage: walleyeSurfPerch,
     diet: 'still under research',
-    catchMethod: 'still under research',
-    regulation: 'still under research',
-    source: 'still under research',
+    catchMethod: 'Carolina Rig with camo gulp worm or sand crab',
+    regulation:
+      'The recreational fishery for surfperch (family Embiotocidae) is open year-round. The daily bag and possession limit is 20 fish in combination of all species (except shiner perch), with not more than 10 fish of any one species. Shiner perch (Cymatogaster aggregata) have a separate bag and possession limit of 20 fish. Redtail surfperch (Amphistichus rhodoterus) have a minimum size limit of 10½ inches total length. ',
+    source:
+      'https://wildlife.ca.gov/Fishing/Ocean/Regulations/Fishing-Map/Southern#surfperch , Indentification guide: https://nrm.dfg.ca.gov/FileHandler.ashx?DocumentID=36617&inlin',
   },
 
   {
@@ -90,9 +95,11 @@ export const SpeciesData = [
     longDescription: '',
     profileImage: californiaHalibut,
     diet: 'still under research',
-    catchMethod: 'still under research',
-    regulation: 'still under research',
-    source: 'still under research',
+    catchMethod:
+      'You can find California Halibut near riptides, shollaw wters with rocky/sandy/muddy bottoms. Popular methods include Drop shot rig with a 5in. white fluke or other swimbait or Lucky craft Flash Minnow 110',
+    regulation:
+      'Halibut must be 22in in length, (3) fish north of Point Sur, Monterey County, and (5) fish south of Point Sur, Monterey County',
+    source: 'https://wildlife.ca.gov/Fishing/Ocean/Regulations/Fishing-Map/Southern#halibut',
   },
 
   {
