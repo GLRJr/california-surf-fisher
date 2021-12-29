e955985abce68ef5b103e74ea144cde2084804d7
theresfuelinmycoffee <atigrouse.com>
2021-12-28 16:49:10 -0800

    fixed beach data

--- src/Components/FullBeachProfile/index.js
+++ src/Components/FullBeachProfile/index.js
@@ -11,14 +11,16 @@ function FullBeachProfile(props) {
       <NavBar />
       {/* i want to add a swipe photo gallery */}
       <img src={spot.profileImage} alt="profile" />
-      <h1>{spot.commonName}</h1>
-      <h3>({spot.scienceName})</h3>
-      <h4>Description</h4>
-      <p>{spot.longDescription}</p>
-      <h4>Tips on catching</h4>
-      <p>{spot.catchMethod}</p>
-      <h4>Regulation</h4>
-      <p>{spot.regulation}</p>
+      <h1>{spot.id}</h1>
+      {/* <h3>({spot.scienceName})</h3> */}
+      {/* <h4>Description</h4>
+      <p>{spot.longDescription}</p> */}
+      <h4>Popular Beaches</h4>
+      <p>{spot.popularBeach}</p>
+      <h4>Piers</h4>
+      <p>{spot.pier.title}</p>
+      <p>{spot.pier.description}</p>
+      <p>{spot.pier.location}</p>
       <h4>Sources</h4>
       <p>{spot.source}</p>
     </main>
